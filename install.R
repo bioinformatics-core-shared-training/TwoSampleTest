e512b686efeb4e7fdeef1197a602288d8dc01834
eldrid01 <matthew.d.eldridge@gmail.com>
2019-02-09 12:01:43 +0000

    Added tidyverse dependency, removed RcmdrMisc

--- install.R
+++ install.R
@@ -1,3 +1,2 @@
 options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
-
-install.packages(c("ggplot2","reshape2","gridExtra","pastecs","tidyr", "RcmdrMisc"))
+install.packages(c("tidyverse", "reshape2", "gridExtra", "pastecs"))
